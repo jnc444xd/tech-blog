a2af1835544b365537cd788aa57bd46016d9c726
[j chun] <[justinchun224@gmail.com]>
2023-10-27 14:22:02 -0700

    finished index.js model

--- models/index.js
+++ models/index.js
@@ -2,3 +2,22 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
+// Each Post belongs to a User
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+// Each Comment belongs to a User
+UserUser.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+});
